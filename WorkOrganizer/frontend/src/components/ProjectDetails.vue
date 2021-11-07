28c0ad4fc2e19f3b8523fbcc78ae9e27ef0a81e0
Børge <borge.rokseth@gmail.com>
2021-11-07 21:54:50 +0100

    Working on interface for adding or editing projects

---
+++ WorkOrganizer/frontend/src/components/ProjectDetails.vue
@@ -0,0 +1,77 @@
+<template>
+  <v-row justify="center">
+    <v-dialog v-model="showDialog" persistent max-width="600px">
+      <v-card>
+        <v-card-title>
+          <span class="headline">{{ title }}</span>
+        </v-card-title>
+        <v-card-text>
+          <v-text-field
+            v-model="projectNewName"
+            label="Project Name"
+            outlined
+            clearable
+          ></v-text-field>
+          <v-textarea
+            outlined
+            label="Project Goal"
+            v-model="projectNewGoal"
+          ></v-textarea>
+          <v-list>
+            <v-header>Actions</v-header>
+            <v-list-item>
+              <v-list-item-content>
+                <v-list-item-title
+                  >Første action lagt til prosjekt</v-list-item-title
+                >
+              </v-list-item-content>
+            </v-list-item>
+            <v-list-item>
+              <v-list-item-content>
+                <v-list-item-title>En annen action...</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list>
+        </v-card-text>
+        <v-card-actions>
+          <v-btn color="blue darken-1" @click="closeDialog()"> Close </v-btn>
+          <v-btn color="blue darken-1"> Save </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: "",
+  props: {
+    showDialog: Boolean,
+    projectId: { type: [Number, null] },
+    projectName: String,
+    goal: String,
+    actions: Array,
+    title: String,
+  },
+  data() {
+    return {
+      projectNewName: "",
+      projectNewGoal: "",
+    };
+  },
+  methods: {
+    closeDialog() {
+      this.$emit("close:dialog", {});
+    },
+    initializeVariables() {
+      this.projectNewName = this.projectName;
+      this.projectNewGoal = this.goal;
+    },
+  },
+  created() {
+    this.initializeVariables();
+  },
+};
+</script>
+
+<style scoped></style>
--- WorkOrganizer/frontend/src/views/ProjectList.vue
+++ WorkOrganizer/frontend/src/views/ProjectList.vue
@@ -1,17 +1,70 @@
 <template>
   <v-container>
-    <h2>Projects</h2>
+    <v-card>
+      <v-card-title>Projects</v-card-title>
+      <v-card-text>
+        <v-simple-table dark>
+          <template v-slot:default>
+            <thead>
+              <tr>
+                <th class="text-left">Name</th>
+                <th class="text-left">Goal</th>
+                <th class="text-left">Date</th>
+                <th></th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="project in activeProjects" :key="project.id">
+                <td>{{ project.name }}</td>
+                <td>{{ project.goal }}</td>
+                <td>{{ project.created }}</td>
+                <td>
+                  <v-icon small @click="editProject(project)">
+                    mdi-pencil
+                  </v-icon>
+                </td>
+                <td>
+                  <v-icon small @click="archiveProject(project)">
+                    mdi-check-outline
+                  </v-icon>
+                </td>
+              </tr>
+            </tbody>
+          </template>
+        </v-simple-table>
+      </v-card-text>
+      <v-card-actions>
+        <v-btn dark @click="newProject()">New project</v-btn>
+      </v-card-actions>
+    </v-card>
+    <project-detail-dialog
+      :show-dialog="showProjectDetailDialog"
+      :project-id="editedProjectId"
+      :project-name="editedProjectName"
+      :goal="editedProjectGoal"
+      :actions="editedProjectActions"
+      :title="dialogTitle"
+      @close:dialog="closeProjectDetailDialog"
+    />
   </v-container>
 </template>
 
 <script>
 import { apiService } from "@/common/api.service";
+import ProjectDetailDialog from "../components/ProjectDetails.vue";
 export default {
   name: "ProjectList",
-  components: {},
+  components: { ProjectDetailDialog },
   data() {
     return {
       activeProjects: [],
+      showProjectDetailDialog: false,
+      editedProjectId: null,
+      editedProjectName: "",
+      editedProjectGoal: "",
+      editedProjectActions: [],
+      dialogTitle: ""
     };
   },
   methods: {
@@ -22,6 +75,24 @@ export default {
         this.activeProjects = data;
       });
     },
+    editProject(project) {
+      console.log("Edit project: ", project.name);
+    },
+    archiveProject(project) {
+      console.log("Archive project: ", project.name);
+    },
+    closeProjectDetailDialog() {
+      this.showProjectDetailDialog = false;
+    },
+    newProject() {
+        this.dialogTitle = "Create New Project";
+        this.editedProjectId = null;
+        this.editedProjectName = "";
+        this.editedProjectGoal = "";
+        this.editedProjectActions = [];
+
+        this.showProjectDetailDialog = true;
+    }
   },
   created() {
     this.getActiveProjects();
